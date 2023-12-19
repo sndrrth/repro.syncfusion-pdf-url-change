e23c516106013979d0c978e1b7d9e090cdb1db23
Sandro Roth <114936395+sndrrth@users.noreply.github.com>
2023-12-19 14:45:34 +0000

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,31 @@
 import { useState } from 'react';
-import { PdfEditor } from './pdf-editor';
+import { PdfViewerComponent,} from "@syncfusion/ej2-react-pdfviewer";
+
+const resourceUrl = `${window.location.origin}/ej2-pdfviewer-lib/24.1.41/`;
+
+const PdfEditor = (props: { pdfUrl: string }) => {
+	const {  pdfUrl } = props;
+	return (
+		<PdfViewerComponent
+			height="500px"
+			width="100%"
+			documentPath={pdfUrl}
+			resourceUrl={resourceUrl}
+		/>
+	);
+};
+
+
 
 function App() {
   const [url, setUrl] = useState("");
 
   return (
     <div style={{ marginTop: 100 }}>
-      <button onClick={() => setUrl("http://localhost:5173/document.pdf")}>Load document.pdf</button>
-      <button onClick={() => setUrl("http://localhost:5173/sample.pdf")}>Load sample.pdf</button>
-      <button onClick={() => setUrl("http://localhost:5173/sample2.pdf")}>Load sample2.pdf</button>
+      <button onClick={() => setUrl(`${window.location.origin}/dummy.pdf`)}>Load dummy.pdf</button>
+      <button onClick={() => setUrl(`${window.location.origin}/dummy2.pdf`)}>Load dummy2.pdf</button>
 
-      <button onClick={() => setUrl("http://localhost:5173/dummy.pdf")}>Load dummy.pdf</button>
-      <button onClick={() => setUrl("http://localhost:5173/dummy2.pdf")}>Load dummy2.pdf</button>
-
-      {!!url && <PdfEditor
-        fileName={url}
-        pdfUrl={url}
-      />}
+      {!!url && <PdfEditor pdfUrl={url} />}
     </div>
   );
 }
